71dd2ce22521d1fc7484477312469ecc464f7845
ArnobMahmud <arnob.tech.me@gmail.com>
2020-10-14 07:55:47 +0600

    Binary Search

--- Binary_Search.cpp
+++ Binary_Search.cpp
@@ -1,26 +1,32 @@
+/*
+    Author : Arnob Mahmud
+
+    Mail : arnob.tech.me@gmail.com
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int middle, searchIndex,
+    int middle, searchItemIndex,
     left = 0,
     right = 9;
 
     int arr[] = {2, 5, 7, 9, 13, 26, 31, 42, 51, 67};
 
     cout << "Enter a value which you want to search : " << endl;
-    cin >> searchIndex;
+    cin >> searchItemIndex;
 
     while (left <= right)
     {
         middle = (left + right) / 2;
-        if (arr[middle] == searchIndex)
+        if (arr[middle] == searchItemIndex)
         {
             cout << "Item found at " << middle << " index" << endl;
             break;
         }
-        else if ( arr[middle] < searchIndex)
+        else if ( arr[middle] < searchItemIndex)
         {
             left = middle + 1;
         }
